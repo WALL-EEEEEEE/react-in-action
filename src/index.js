d1779c60e7bdcece21ccae68444d4a2471c14cc0
Wall'e <duanqingbb@163.com>
2024-07-09 18:36:13 +0800

    add comment component as post comment's subcomponent

--- src/index.js
+++ src/index.js
@@ -1,47 +1,88 @@
-import React, { Component } from 'react';
-import {createRoot} from 'react-dom/client';
+import React, { Component } from "react";
+import { createRoot } from "react-dom/client";
 import PropTypes from "prop-types";
 
-const node = document.getElementById('root')
-const root = createRoot(node)
+const node = document.getElementById("root");
+const root = createRoot(node);
 
 //通过继承`React`中提供的`Component`的API来自定义一个组件
 class Post extends Component {
-
-    //通过实现`render`接口实现视图生成逻辑
-    render() {
-      return React.createElement(
-        "div", 
+  //通过实现`render`接口实现视图生成逻辑
+  render() {
+    return React.createElement(
+      "div",
+      {
+        className: "post",
+      },
+      React.createElement(
+        "h2",
         {
-          className: "post"
+          className: "postAuthor",
+          id: this.props.id,
         },
+        this.props.user,
         React.createElement(
-          "h2",
+          "span",
           {
-            className: "postAuthor",
-            id: this.props.id
+            className: "postBody",
           },
-          this.props.user,
-          React.createElement(
-            "span",
-            {
-              className: "postBody"
-            },
-            this.props.content
-          )
+          this.props.content
+        ),
+        this.props.children
+      )
+    );
+  }
+}
+
+class Comment extends Component {
+  render() {
+    console.log("yo");
+    return React.createElement(
+      "div",
+      {
+        className: "comment",
+      },
+      React.createElement(
+        "h2",
+        {
+          className: "commentAuthor",
+        },
+        this.props.user,
+        React.createElement(
+          "span",
+          {
+            className: "commentContent",
+          },
+          this.props.content
         )
       )
-    }
+    );
+  }
 }
+
+Comment.propTypes = {
+  id: PropTypes.number.isRequired,
+  content: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired,
+};
+
 //通过指定propTypes属性，指定组件内部数据的校验和规范
 Post.propTypes = {
   user: PropTypes.string.isRequired,
   content: PropTypes.string,
-  id: PropTypes.number.isRequired
+  id: PropTypes.number.isRequired,
 };
-const App = React.createElement(Post, {
-  id: 1, 
-  content: " said: This is a post!",
-  user: "mark"
-});
-root.render(App)
+const App = React.createElement(
+  Post,
+  {
+    id: 1,
+    content: " said: This is a post!",
+    user: "mark",
+  },
+  React.createElement(Comment, {
+    id: 2,
+    user: "bob",
+    content: " commented: wow! how cool!",
+  })
+);
+root.render(App);
