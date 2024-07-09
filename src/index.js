79c58ce9c37341aca0f82e379fac8719682824e1
Wall'e <duanqingbb@163.com>
2024-07-09 17:19:36 +0800

    add some comment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import PropTypes from "prop-types";
 const node = document.getElementById('root')
 const root = createRoot(node)
 
+//通过继承`React`中提供的`Component`的API来自定义一个组件
 class Post extends Component {
+
+    //通过实现`render`接口实现视图生成逻辑
     render() {
       return React.createElement(
         "div", 
@@ -30,6 +33,7 @@ class Post extends Component {
       )
     }
 }
+//通过指定propTypes属性，指定组件内部数据的校验和规范
 Post.propTypes = {
   user: PropTypes.string.isRequired,
   content: PropTypes.string,
