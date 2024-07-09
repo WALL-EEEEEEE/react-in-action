c67058de0fb819fed6e7c86ad4b470fe4d18592e
Wall'e <duanqingbb@163.com>
2024-07-09 17:09:09 +0800

    finish action-2.4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {createRoot} from 'react-dom/client';
+import PropTypes from "prop-types";
 
 const node = document.getElementById('root')
 const root = createRoot(node)
-const n = React.createElement(
-  "div", 
-  {},
-  "Hello, World!",
-  React.createElement(
-    "a",
-    {href: "mailto:mark@ifelse.io"},
-    React.createElement("h1", {}, "React In Action"),
-    React.createElement("em", {}, "...and now it really is!")
-  )
-)
-root.render(n)
+
+class Post extends Component {
+    render() {
+      return React.createElement(
+        "div", 
+        {
+          className: "post"
+        },
+        React.createElement(
+          "h2",
+          {
+            className: "postAuthor",
+            id: this.props.id
+          },
+          this.props.user,
+          React.createElement(
+            "span",
+            {
+              className: "postBody"
+            },
+            this.props.content
+          )
+        )
+      )
+    }
+}
+Post.propTypes = {
+  user: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  id: PropTypes.number.isRequired
+};
+const App = React.createElement(Post, {
+  id: 1, 
+  content: " said: This is a post!",
+  user: "mark"
+});
+root.render(App)
